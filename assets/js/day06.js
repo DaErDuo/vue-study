3e951965a7b366a66f6092d56c2105bdd6c72345
DaErDuo <bigearguy@gmail.com>
2017-06-03 01:41:32 +0800

    finish day06

--- assets/js/day06.js
+++ assets/js/day06.js
@@ -1,6 +1,7 @@
 /**
  * Created by Sean on 2017/5/18.
  */
+// input
 var demo = new Vue({
     el: '#demo-0',
     data: {
@@ -8,9 +9,118 @@ var demo = new Vue({
     }
 });
 
+// textarea
 var demo1 = new Vue({
     el: '#demo-1',
     data: {
         message: ''
     }
 });
+
+// checkbox
+var demo2 = new Vue({
+    el: '#demo-2',
+    data: {
+        checked: false
+    }
+});
+
+// checkbox
+var demo3 = new Vue({
+    el: '#demo-3',
+    data: {
+        checkedNames: []
+    }
+});
+
+// radio
+var demo4 = new Vue({
+    el: '#demo-4',
+    data: {
+        picked: ''
+    }
+});
+
+// select
+var demo5 = new Vue({
+    el: '#demo-5',
+    data: {
+        selected: ''
+    }
+});
+
+// select multiple
+var demo6 = new Vue({
+    el: '#demo-6',
+    data: {
+        selected: ['2', '3']
+    }
+});
+
+// select multiple
+var demo7 = new Vue({
+    el: '#demo-7',
+    data: {
+        selected: '1',
+        options: [
+            {text: 'A', value: 1},
+            {text: 'B', value: 2},
+            {text: 'C', value: 3},
+            {text: 'D', value: 4}
+        ]
+    }
+});
+
+var demo8 = new Vue({
+    el: '#demo-8',
+    data: {
+        picked: false,
+        toggle: null,
+        selected: null
+    }
+});
+
+var demo9 = new Vue({
+    el: '#demo-9',
+    data: {
+        a: true,
+        b: false,
+        foo: 'abc',
+        picked: false,
+        toggle: null,
+        selected: null
+    }
+});
+
+var demo10 = new Vue({
+    el: '#demo-10',
+    data: {
+        msg: 'abc',
+        msg1: '  xyz',
+        age: 0
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
